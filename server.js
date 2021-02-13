2adacdc109a0dfd68369bc6ad1d6123bc42ed9d8
Seth Staggs <sethstaggs2@gmail.com>
2021-02-13 14:11:28 -0600

    added npm modules and fixed server

--- server.js
+++ server.js
@@ -1,15 +1,9 @@
-require("dotenv").config();
 const express = require("express");
-const logger = require("morgan");
-const mongojs = require("mongojs");
 const mongoose = require("mongoose");
-const path = require("path");
 
 const PORT = process.env.PORT || 3000;
 
-const User = require("./models/models");
 const app = express();
-app.use(logger("dev"));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
